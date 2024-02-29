ecc65d5873112c610443974b1b6fd31c29e3243d
Elias Bourgess <ebourgess@gmail.com>
2024-02-29 21:44:50 +0100

    Create pass-generator.go

---
+++ README.md
@@ -0,0 +1,61 @@
+# Password Generator
+
+A simple password generator written in Go.
+
+## Description
+
+This script generates random passwords based on user-defined criteria such as length and character types.
+
+## Features
+
+- Customizable password length
+- Option to include uppercase letters, lowercase letters, numbers, and special characters
+- Secure and random password generation
+
+## Installation
+
+1. Clone the repository:
+
+    ```shell
+    git clone https://github.com/ebourgess/pass-generator.git
+    ```
+
+2. Build the executable:
+
+    ```shell
+    go build pass-generator.go
+    ```
+
+3. To use it globally move it to `/usr/local/bin`
+
+    ```shell
+    sudo mv pass-generator /usr/local/bin
+    ```
+
+## Usage
+
+```shell
+pass-generator -h
+    --digits
+        Use digits
+    --length int
+        Password length (default 8)
+    --lower
+        Use lowercase letters
+    --special
+        Use special characters
+    --upper
+        Use uppercase letters
+```
+
+In order to use this simply run the command
+
+```shell
+pass-generator --length 8 --digits --lower --special --upper
+```
+
+While the length has a default value and not required here, **At least ONE of the other 4 is required to generate the password/secret phrase**
+
+## License
+
+This project is licensed under the MIT License. See the [LICENSE](LICENSE) file for more information.
---
+++ pass-generator.go
@@ -0,0 +1,73 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"math/rand"
+	"time"
+)
+
+var (
+	length     = flag.Int("length", 8, "Password length")
+	useLower   = flag.Bool("lower", false, "Use lowercase letters")
+	useUpper   = flag.Bool("upper", false, "Use uppercase letters")
+	useDigits  = flag.Bool("digits", false, "Use digits")
+	useSpecial = flag.Bool("special", false, "Use special characters")
+)
+
+const (
+	lowercase = "abcdefghijklmnopqrstuvwxyz"
+	uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+	digits    = "0123456789"
+	special   = "!@#$%^&*()-_=+,.?/:;{}[]`~"
+)
+
+func generatePassword() string {
+	var charset string
+
+	if *useLower {
+		charset += lowercase
+	}
+
+	if *useUpper {
+		charset += uppercase
+	}
+
+	if *useDigits {
+		charset += digits
+	}
+
+	if *useSpecial {
+		charset += special
+	}
+
+	if charset == "" {
+		panic("No character sets selected")
+	}
+
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	password := make([]byte, *length)
+	for i := range password {
+		password[i] = charset[r.Intn(len(charset))]
+	}
+
+	return string(password)
+}
+
+func main() {
+	flag.Parse()
+
+	if *length <= 0 {
+        fmt.Println("Error: Password length must be greater than 0.")
+        return
+    }
+
+	if !(*useLower || *useUpper || *useDigits || *useSpecial) {
+        fmt.Println("Error: At least one character set must be selected.")
+		fmt.Println("Use -h for help.")
+        return
+    }
+
+	password := generatePassword()
+	fmt.Println(password)
+}
